943c1a06a77b9920ec2b556813b737ee49911c35
Roman Kuzmin <nightroman@gmail.com>
2022-10-13 12:22:08 +0100

    Add sample fn-commands to script methods.

--- ScriptCSharp/Demo.cs
+++ ScriptCSharp/Demo.cs
@@ -4,6 +4,7 @@ namespace ScriptCSharp;
 
 public static class Demo
 {
+	// fn: script=ScriptCSharp; method=ScriptCSharp.Demo.Message; unload=true :: name=John Doe; age=42
 	public static void Message(string name = "unknown", int age = -1)
 	{
 		Far.Api.Message($"name: {name}, age: {age}");
--- ScriptFSharp/Demo.fs
+++ ScriptFSharp/Demo.fs
@@ -1,5 +1,6 @@
 ﻿module ScriptFSharp.Demo
 open FarNet
 
+// fn: script=ScriptFSharp; method=ScriptFSharp.Demo.Message; unload=true :: name=John Doe; age=42
 let Message (name: string) (age: int) =
     far.Message($"name: {name}, age: {age}")
